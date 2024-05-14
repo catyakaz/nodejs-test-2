657703d0d23b1c5571fedd0ef167e8a90c01e270
EKATERINA <eakazakova@domclick.ru>
2024-05-14 14:27:51 +0300

    nodejs test2

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,4 +1,4 @@
-import { PostsService } from './posts.service';
+import { PostsService, FindManyOptions } from './posts.service';
 
 describe('PostsService', () => {
   let postsService: PostsService;
@@ -20,13 +20,58 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const findManyPosts = postsService.findMany();
+      const postsLength = findManyPosts.length;
+      const result = {
+        id: `${postsLength}`,
+        text: 'Post 4',
+      };
+
+      expect(postsLength).toBe(postsLength);
+      expect(findManyPosts[postsLength - 1]).toHaveProperty('text');
+      expect(findManyPosts[postsLength - 1]).toEqual(result);
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const options: FindManyOptions = { skip: 2, limit: 1 };
+      const findManyPosts = postsService.findMany(options);
+      const postsLength = findManyPosts.length;
+      const result = {
+        id: '3',
+        text: 'Post 3',
+      };
+
+      expect(postsLength).toBe(1);
+      expect(findManyPosts[0]).toHaveProperty('text');
+      expect(findManyPosts[0]).toEqual(result);
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('should return correct posts for skip options', () => {
+      const options: FindManyOptions = { skip: 2 };
+      const findManyPosts = postsService.findMany(options);
+      const postsLength = findManyPosts.length;
+      const result = {
+        id: '3',
+        text: 'Post 3',
+      };
+
+      expect(postsLength).toBe(2);
+      expect(findManyPosts[0]).toHaveProperty('text');
+      expect(findManyPosts[0]).toEqual(result);
+    });
+
+    it('should return correct posts for limit options', () => {
+      const options: FindManyOptions = { limit: 3 };
+      const findManyPosts = postsService.findMany(options);
+      const postsLength = findManyPosts.length;
+      const result = {
+        id: '1',
+        text: 'Post 1',
+      };
+
+      expect(postsLength).toBe(3);
+      expect(findManyPosts[0]).toHaveProperty('text');
+      expect(findManyPosts[0]).toEqual(result);
+    });
   });
 });
